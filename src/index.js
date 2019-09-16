71585bfea232e28f1076b88f7beb7c5cc05bf0ca
gutianci <gutianci@pwrd.com>
2019-09-16 18:48:28 +0800

    fix: activePageId变化而Page不变的问题

--- src/index.js
+++ src/index.js
@@ -528,6 +528,7 @@ class App extends React.Component {
     }
 
     render() {
+        // 注：Page外层包上一层div的作用：避免Page和SideBar的key值冲突。
         return (<div
             style={{
                 height: '100%',
@@ -537,14 +538,18 @@ class App extends React.Component {
                 gridTemplateColumns: '80% 20%'
             }}>
               <TopBar
-                 activeSectionId={this.state.activeSectionId}
-                 onSectionClick={this.handleSectionClick} />
-              <Page activePageId={this.state.activePageId} />
+                activeSectionId={this.state.activeSectionId}
+                onSectionClick={this.handleSectionClick} />
+              <div>
+                <Page
+                  key={this.state.activePageId} //如果activePageId变了，则Page直接re-create。
+                  activePageId={this.state.activePageId} />
+              </div>
               <SideBar
-                 key={this.state.activeSectionId} //如果activeTopBarNodeId变了，则SideBar直接re-create。
-                 sectionId={this.state.activeSectionId}
-                 activePageId={this.state.activePageId}
-                 onPageClick={this.handlePageNodeClick}/>
+                key={this.state.activeSectionId} //如果activeTopBarNodeId变了，则SideBar直接re-create。
+                sectionId={this.state.activeSectionId}
+                activePageId={this.state.activePageId}
+                onPageClick={this.handlePageNodeClick}/>
         </div>);
     }
 }
